9c6777d6059e010b6340ffe1f0aa58c9d9c1db87
Eray Sahin <eraysahinmoon@gmail.com>
2023-03-03 23:48:40 -0500

    source: PedroTech YT

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { ShoppingCart } from 'phosphor-react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
 import "./navbar.css";
 
-function navbar() {
+export const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="links">
         <Link to="/"> Shop </Link>
-        <Link to="/cart"> 
-            <ShoppingCart size={32}/>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
         </Link>
       </div>
     </div>
-  )
-}
-
-export default navbar
+  );
+};
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./shop.css";
 
-const Shop = () => {
+export const Shop = () => {
   return (
-    <div>
-        <div className="shop">
-            <div className="shopTitle">
-                <h1>ErayReact Shop</h1>
-            </div>
-        </div>
-        <div className="products">
-            
-        </div>
-    </div>
-  )
-}
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>PedroTech Shop</h1>
+      </div>
 
-export default Shop
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
