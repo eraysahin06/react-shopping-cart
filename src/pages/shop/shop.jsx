9295c02f36ed1af3517d22a9bd14b8b16eda7dca
Eray Sahin <eraysahinmoon@gmail.com>
2023-03-03 23:37:10 -0500

    created products-images

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <div className="shop">
+            <div className="shopTitle">
+                <h1>ErayReact Shop</h1>
+            </div>
+        </div>
+        <div className="products">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Shop
